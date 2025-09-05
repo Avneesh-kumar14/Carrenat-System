7967313170d3913958f393829f1b95507d916ecf
Delta Student <rajneeshavneeshkar@gmail.com>
2025-09-05 14:42:37 +0530

    clean and clear code

--- backend/db.js
+++ backend/db.js
@@ -1,29 +1,3 @@
-// const mongoose = require("mongoose");
-
-// require('dotenv').config();
-// // console.log(process.env.MONGOURL)
-
-// function connectDB(){
-
-//     mongoose.connect(process.env.MONGOURL , {useUnifiedTopology: true , useNewUrlParser: true})
-
-//     const connection = mongoose.connection
-
-//     connection.on('connected' , ()=>{
-//         console.log('Mongo DB Connection Successfull')
-//     })
-
-//     connection.on('error' , ()=>{
-//         console.log('Mongo DB Connection Error')
-//     })
-
-
-// }
-
-// connectDB()
-
-// module.exports = mongoose
-
 
 
 const mongoose = require("mongoose");
--- frontend/src/redux/reducers/bookingsReducer.js
+++ frontend/src/redux/reducers/bookingsReducer.js
@@ -1,25 +1,4 @@
-// const initialData = {
-//     bookings : [],
 
-// };
-
-// export const bookingsReducer = (state=initialData , action)=>{
-
-//      switch(action.type)
-//      {
-//          case 'GET_ALL_BOOKINGS' : {
-//              return{
-//                  ...state,
-//                  bookings : action.payload
-//              }
-//          }
-         
-//          default:return state
-//      }
-
-// }  
-
-// frontend/src/redux/reducers/bookingsReducer.js
 const initialData = {
   bookings: [],
 };
