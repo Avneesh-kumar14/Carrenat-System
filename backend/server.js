685bb9360743f4ab17aef1861c7af19bc971f727
Avneesh kumar <rajneeshavneeshkar@gmail.com>
2025-10-22 13:18:39 +0530

    Refactor server.js for clarity and structure

--- backend/server.js
+++ backend/server.js
@@ -4,28 +4,26 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
-const mongoose = require("./db"); // <-- this exports mongoose
-const User = require("./models/userModel"); // to check/create admin
+const mongoose = require("./db"); 
+const User = require("./models/userModel");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use("/api/cars", require("./routes/carsRoute"));
 app.use("/api/users", require("./routes/usersRoute"));
 app.use("/api/bookings", require("./routes/bookingsRoute"));
 
-// Once DB is connected, check/create default admin
+
 mongoose.connection.once("open", async () => {
   try {
     const adminExists = await User.findOne({ username: "admin" });
     if (!adminExists) {
       const adminUser = new User({
         username: "admin",
-        password: "admin123", // you can change this later
+        password: "admin123", 
         role: "admin",
       });
       await adminUser.save();
@@ -38,7 +36,7 @@ mongoose.connection.once("open", async () => {
   }
 });
 
-// Deployment setup
+
 if (process.env.NODE_ENV === "production") {
   __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "frontend", "build")));
@@ -52,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Start server
+
 app.listen(port, () =>
   console.log(`🚀 Node JS Server started on port ${port}`)
 );
